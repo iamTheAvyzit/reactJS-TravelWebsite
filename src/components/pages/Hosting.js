72961f80ec8fac3afb95919b892683b5e90a3355
Abhijeet <94052679+iamTheAvyzit@users.noreply.github.com>
2021-12-20 00:32:27 -0500

    Add files via upload

---
+++ src/components/pages/Hosting.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Hosting() {
+  return <h5 className='hosting'>Earn extra income and unlock new opportunities by sharing your space.</h5>;
+}
